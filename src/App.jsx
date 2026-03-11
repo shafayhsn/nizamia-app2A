593458530ce1fa7e5586edb97321032579e79ca6
shafayhsn <shafayhsn@gmail.com>
2026-03-11 12:48:15 +0500

    v3

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Layout from './components/layout/Layout'
+import Dashboard from './pages/Dashboard'
+import Orders from './pages/Orders'
+import Purchasing from './pages/Purchasing'
+import Buyers from './pages/Buyers'
+import Suppliers from './pages/Suppliers'
+import Library from './pages/Library'
+import Settings from './pages/Settings'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Navigate to="/dashboard" replace />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="orders/*" element={<Orders />} />
+        <Route path="purchasing/*" element={<Purchasing />} />
+        <Route path="buyers" element={<Buyers />} />
+        <Route path="suppliers" element={<Suppliers />} />
+        <Route path="library" element={<Library />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+    </Routes>
+  )
+}
